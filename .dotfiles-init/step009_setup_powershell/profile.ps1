2848d3142c2419d2804191f5310930f33843b1e2
03 <kntaco03g1@gmail.com>
2024-12-31 21:49:34 +0900

    add PowerShell setup script

---
+++ .dotfiles-init/step009_setup_powershell/profile.ps1
@@ -0,0 +1,23 @@
+#!/usr/bin/env pwsh
+
+function Set-PSReadLineOptions {
+    Import-Module PSReadLine
+
+    # Reference:
+    #   https://learn.microsoft.com/ja-jp/powershell/module/psreadline/set-psreadlineoption
+    $options = @{
+        EditMode = "Emacs"
+        # EditMode = "Windows"
+        HistoryNoDuplicates = $true
+        HistorySearchCursorMovesToEnd = $true
+        MaximumHistoryCount = 10000
+    }
+    Set-PSReadLineOption @options
+
+    # Reference:
+    #   https://learn.microsoft.com/ja-jp/powershell/scripting/learn/shell/using-keyhandlers
+    Set-PSReadLineKeyHandler -Key Ctrl+d -Function DeleteCharOrExit
+    Set-PSReadLineKeyHandler -Key Ctrl+v -Function Paste
+}
+
+Set-PSReadLineOptions
---
+++ .dotfiles-init/step009_setup_powershell/windows.ps1
@@ -0,0 +1,50 @@
+#!/usr/bin/env pwsh
+
+Set-StrictMode -Version 3.0
+$ErrorActionPreference = "Stop"
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+
+function Update-PowerShell {
+    Update-WinGetPackage "Microsoft.PowerShell"
+
+    # Reference:
+    #   https://learn.microsoft.com/ja-jp/powershell/scripting/install/installing-powershell-on-windows
+    $pwshPath = "${env:ProgramFiles}\PowerShell\7"
+    if (-not (${env:Path}.Split(';') -contains "${pwshPath}")) {
+        [Environment]::SetEnvironmentVariable("Path", "${pwshPath};${env:Path}", "User")
+    }
+    Import-Path
+
+    Write-CommandLog { pwsh --version }
+    Write-CommandLog { ${Host} }
+}
+
+function Update-Profile {
+    Write-CommandLog { ${PROFILE} | Format-List -Force }
+    Copy-Resource -Source "profile.ps1" -Target ${PROFILE}.CurrentUserAllHosts
+}
+
+function Update-ExecutionPolicy {
+    Set-ExecutionPolicy -ExecutionPolicy "RemoteSigned" -Scope "CurrentUser"
+    Write-Log
+    Write-CommandLog { Get-ExecutionPolicy -List }
+}
+
+function Step9 {
+    Write-Step 9 "Setup PowerShell"
+
+    Update-PowerShell
+    Update-Profile
+    Update-ExecutionPolicy
+
+    . ${PROFILE}.CurrentUserAllHosts
+
+    # Write-CommandLog { Get-ChildItem "${env:APPDATA}\Microsoft\Windows\PowerShell\PSReadLine" }
+    # Write-CommandLog { Get-PSReadLineOption }
+    # Write-CommandLog { Get-PSReadLineKeyHandler }
+}
+
+if ((-not ${MyInvocation}.ScriptName) -or (${MyInvocation}.ScriptName -ne "${PSCommandPath}")) {
+    Import-Module -Name "$(Join-Path ("${PSCommandPath}" | Split-Path | Split-Path) "windows_utils.psm1")" -Force
+    Step9 -Args $args
+}
