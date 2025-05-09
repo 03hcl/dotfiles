eed6673a8c3553e7906d77b2e36e089ff95c2a02
03 <okude@isp.co.jp>
2025-05-09 12:02:59 +0900

    add Linux-like ssh-copy-id function to PowerShell profile

--- .dotfiles-init/step006_setup_powershell/profile.ps1
+++ .dotfiles-init/step006_setup_powershell/profile.ps1
@@ -22,9 +22,27 @@ function Set-PSReadLineOptions {
     Set-PSReadLineKeyHandler -Key Ctrl+RightArrow -Function ForwardWord
 }
 
+function Invoke-LinuxLikeSSHCopyId {
+    [CmdletBinding()]
+    param (
+        [Alias("i")]
+        [Parameter(Mandatory = $true)]
+        [string]$PubKeyPath,
+        [Parameter(ValueFromRemainingArguments = $true)]
+        [string[]]$SSHParams,
+        [Parameter(Mandatory = $true)]
+        [string[]]$Destination
+    )
+    $content = (Get-Content -Path "${PubKeyPath}" -ErrorAction Stop) -replace "`r?`n", ""
+    $command = "mkdir -p ~/.ssh && chmod 700 ~/.ssh && printf '%s\n' '${content}' >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys"
+    ssh ${SSHParams} "${Destination}" "${command}"
+}
+
 chcp 65001
 
 Set-PSReadLineOptions
+
+New-Alias -Name ssh-copy-id -Value Invoke-LinuxLikeSSHCopyId -Option AllScope
 
 ""
 "==============================================================================="
